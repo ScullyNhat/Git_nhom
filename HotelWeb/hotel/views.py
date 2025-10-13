84c0f7861b748c38b73ee7c351d6a3d146986663
kuuhaku93 <0306231432@caothang.edu.vn>
2025-10-13 23:17:46 +0700

    LẬP TRÌNH CHỨC NĂNG HIỂN THỊ DANH SÁCH KHÁCH SẠN

--- HotelWeb/hotel/views.py
+++ HotelWeb/hotel/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render,redirect,HttpResponse
+from django.views import View
+from .models import Hotel,User
+from .forms import AddHotel,AddUser
+
+class Index(View):
+    def get(self,request):
+        return render(request,"hotel/index.html")
+    
+class DanhSach(View):
+    def get(seft,request):
+        h = Hotel.objects.filter(Status=True)
+        context={"hotel":h}
+        return render(request,"hotel/danhsach.html",context)

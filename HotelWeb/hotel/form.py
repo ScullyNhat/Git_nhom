5901f23cb39b7fb7a4e0444eca5e4969621646db
kuuhaku93 <0306231432@caothang.edu.vn>
2025-10-13 23:05:26 +0700

    TẠO FORM USER

--- HotelWeb/hotel/form.py
+++ HotelWeb/hotel/form.py
@@ -11,3 +11,14 @@ class AddHotel(forms.ModelForm):
             'Rating': forms.NumberInput(attrs={'id':'Rating','class':'form-control'}),
             'PricePer': forms.NumberInput(attrs={'id':'PricePer','class':'form-control'})
         }
+
+class AddUser(forms.ModelForm):
+    BookingRoom= forms.ModelChoiceField(queryset=Hotel.objects.filter(RoomAvilable__gt=0,Status=True).distinct(), widget=forms.Select(attrs={'id': 'BookingRoom', 'class': 'form-control'}))
+    class Meta:
+        model=User
+        fields=('UserName','UserID','BookingCost','BookingRoom')
+        widgets={
+            'UserName': forms.TextInput(attrs={'id':'UserName','class':'form-control'}),
+            'UserID': forms.NumberInput(attrs={'id':'UserID','class':'form-control'}),
+            'BookingCost': forms.NumberInput(attrs={'id':'BookingCost','class':'form-control'}),
+            }

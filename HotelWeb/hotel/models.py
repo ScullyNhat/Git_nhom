a4eaf784863e92e4af03b4be0cf992ae1257ef4e
kuuhaku93 <0306231432@caothang.edu.vn>
2025-10-13 22:59:37 +0700

    TẠO MODEL USER

--- HotelWeb/hotel/models.py
+++ HotelWeb/hotel/models.py
@@ -9,3 +9,12 @@ class Hotel(models.Model):
     Status=models.BooleanField(default=True,null=False,blank=False)
     def __str__(self):
         return self.HotelName
+
+class User(models.Model):
+    UserName=models.CharField(max_length=200,null=False,blank=False)
+    UserID=models.IntegerField(unique=True,null=False,blank=False)
+    BookingCost=models.IntegerField(default=0,null=False,blank=False)
+    BookingRoom=models.CharField(max_length=200,null=False,blank=False)
+    Status=models.BooleanField(default=True,null=False,blank=False)
+    def __str__(self):
+        return self.UserName
